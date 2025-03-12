a2d150d79d3e2d4ee8eb9571e6edc8491d09200d
NicMICHEL <nic.chel@gmail.com>
2025-03-12 18:24:23 +0100

    2e test commit

--- mod1/src/main/java/com/nm/mod1/model/DataDTO.java
+++ mod1/src/main/java/com/nm/mod1/model/DataDTO.java
@@ -4,25 +4,49 @@ import java.util.List;
 
 public class DataDTO {
 
-    private String firstNameToSearch;
+    private String firstName;
     private String lastNameToSearch;
-    private String patient;
+    private int patient;
     private List<String> notesDTOs;
     //private NoteBean noteDTO;
 
     private String noteText;
     private String riskLevel;
 
-    public DataDTO() {
-    }
 
-    public DataDTO(String firstNameToSearch, String lastNameToSearch, String patient,
+
+    public DataDTO(String firstName, String lastNameToSearch, int patient,
                    List<String> notesDTOs, String noteText, String riskLevel) {
-        this.firstNameToSearch = firstNameToSearch;
+        this.firstName = firstName;
         this.lastNameToSearch = lastNameToSearch;
         this.patient = patient;
         this.notesDTOs = notesDTOs;
         this.noteText = noteText;
         this.riskLevel = riskLevel;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNameToSearch() {
+        return lastNameToSearch;
+    }
+
+    public void setLastNameToSearch(String lastNameToSearch) {
+        this.lastNameToSearch = lastNameToSearch;
+    }
+
+    public int getPatient() {
+        return patient;
+    }
+
+    public void setPatient(int patient) {
+        this.patient = patient;
     }
 }
